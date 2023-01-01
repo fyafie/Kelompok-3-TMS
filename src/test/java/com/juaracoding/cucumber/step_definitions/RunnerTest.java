8d0056f67926b6f04f118e02f07b16c63a2a810f
fyafie <95351306+fyafie@users.noreply.github.com>
2023-01-01 17:11:35 +0700

    new

--- src/test/java/com/juaracoding/cucumber/step_definitions/RunnerTest.java
+++ src/test/java/com/juaracoding/cucumber/step_definitions/RunnerTest.java
@@ -4,7 +4,12 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(tags = "",
-        features = {"src/main/resources/features/01.Login.feature"},
+        features = {
+        		"src/main/resources/features/01.Login.feature",
+        		"src/main/resources/features/07.ParameterShifting.feature",
+        		"src/main/resources/features/08.KerjaPenempatan.feature",
+        		"src/main/resources/features/09.MasterPosition.feature",
+        		},
         glue = "com.juaracoding.cucumber.step_definitions",
         plugin = {"pretty","html:target/cucumber-reports.html","json:target/cucumber.json"})
 public class RunnerTest extends AbstractTestNGCucumberTests {
