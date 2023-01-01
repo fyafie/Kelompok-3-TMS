ba5790fb87b25c9e3f3af8d9f7fece9fb6df7e93
fyafie <95351306+fyafie@users.noreply.github.com>
2023-01-01 18:38:45 +0700

    new

--- src/main/java/com/juaracoding/cucumber/utils/Constants.java
+++ src/main/java/com/juaracoding/cucumber/utils/Constants.java
@@ -9,5 +9,7 @@ public class Constants {
     public static final String URLShfting = "https://dev.ptdika.com/tiket.com/parameter_shift/";
     public static final String URLPenempatan = "https://dev.ptdika.com/tiket.com/wpu/master_wpu";
     public static final String URLPosition = "https://dev.ptdika.com/tiket.com/position_tms/master_p_tms";
+    public static final String URLtambahDivisi = "https://dev.ptdika.com/tiket.com/admin/master_divisi";
+    public static final String URLmanagemenDivisi = "https://dev.ptdika.com/tiket.com/admin/divisi";
     public static final int DETIK = 1;
 }
