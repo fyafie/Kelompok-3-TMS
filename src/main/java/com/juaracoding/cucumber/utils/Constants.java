f16cdfd6f63152bdb6457eac7bf419e776d0c8b5
fyafie <95351306+fyafie@users.noreply.github.com>
2023-01-01 17:03:54 +0700

    new

--- src/main/java/com/juaracoding/cucumber/utils/Constants.java
+++ src/main/java/com/juaracoding/cucumber/utils/Constants.java
@@ -5,6 +5,9 @@ public class Constants {
     public static final String CHROME = "Chrome";
     public static final String FIREFOX = "Firefox";
     public static final String URL = "https://dev.ptdika.com/tiket.com/login";
-//    public static final String URL = "https://dev.ptdika.com/siloam/panel/login/";
+    public static final String URL2 = "https://dev.ptdika.com/tiket.com/loginn";
+    public static final String URLShfting = "https://dev.ptdika.com/tiket.com/parameter_shift/";
+    public static final String URLPenempatan = "https://dev.ptdika.com/tiket.com/wpu/master_wpu";
+    public static final String URLPosition = "https://dev.ptdika.com/tiket.com/position_tms/master_p_tms";
     public static final int DETIK = 1;
 }
